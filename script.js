fe17447b11d3001112647ed38b344a3f4b50c51f
Cash America <cashamerica@Cashs-MacBook-Pro.local>
2026-02-21 09:15:11 +0300

    updated colour and image

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
-function checkResult() {
+function checkResult() 
+{
 let marks = document.getElementById("marksInput").value;
 marks = Number(marks); //converts number
-let resultMessage ="";
+let resultMessage =" ";
 if (marks >= 80) {
     resultMessage = "Congragulation You Passed";
 } else if(marks >= 50) {
-    resultMessage = "Average Score please try hard next time";
-} else {
-    resultMessage = "Come to my Office";
+    resultMessage = "Average Score please try hard next time(did you get 6 7 (:  )";
+}else if(marks > 100) {
+    resultMessage = "please be resoneble and enter a possible mark";
+} else if(marks < 50) {
+resultMessage = "you need to leave(your expelded this school doesnt take students with marks less than 50)";
 }
-
 document.getElementById("resultText").textContent = resultMessage;
-
 }
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
